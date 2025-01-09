72ae5e6a7658683830e753e0550a0c8200d3b054
Alexander Authors <alex1authors@gmail.com>
2025-01-10 10:10:28 +1100

    enabled same wifi network chat ability

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const server = http.createServer(app);
 const io = new Server(server);
 const port = 3000;
 
+// Server the HMTL file 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+// Handle socket events
 io.on('connection', (socket) => {
     socket.on('send name', (username) => {
         io.emit('send name', (username));
@@ -20,6 +22,7 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(port, () => {
+// Bind server to 0.0.0.0 to make it accessible on the local network
+server.listen(port, '0.0.0.0', () => {
     console.log(`Server is listening at the port: ${port}`);
 });
